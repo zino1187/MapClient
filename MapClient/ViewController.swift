cea7828d7d2072bda919ba8c673a7b95e30dfaa3
zino <minzino@hanmail.net>
2020-03-11 17:10:33 +0900

    commit

--- MapClient/ViewController.swift
+++ MapClient/ViewController.swift
@@ -10,9 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var t_lat: UITextField!
+    @IBOutlet weak var t_lng: UITextField!
+    @IBOutlet weak var t_content: UITextField!
+
+    
+    //HTTP POST 요청을 시도하자!!
+    @IBAction func btnClick(_ sender: Any) {
+        
+        let url = URL(string: "http://localhost:9999/regist")
+        
+        var request = URLRequest(url: url!)
+        
+        //딕셔너리
+        let params = [
+          "lati":t_lat.text!,
+          "longi":t_lng.text!,
+          "content":t_content.text!
+        ]
+        
+          request.httpMethod="post"
+        
+          request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+          request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+          do{
+          request.httpBody = try JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
+          }catch{
+              print(error)
+          }
+          
+          //비동기 요청
+          let urlSession = URLSession.shared
+        
+          let task = urlSession.dataTask(with: request, completionHandler: {
+              (data, response,error) in
+          })
+          
+          task.resume()
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
